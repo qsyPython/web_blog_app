cb8cfd9b69fa0151ba05d58d94cf0747eb6fd6c0
midiexiang <midiexiang@7lk.com>
2018-09-11 17:50:21 +0800

    修改host为云服务器的ip，port为22

--- python3-webapp/www/app.py
+++ python3-webapp/www/app.py
@@ -275,8 +275,8 @@ def init(loop):
     logging.info('server before...')
 
     # 创建1个服务器对象
-    host ='10.9.3.240'  #'localhost' == '127.0.0.1'
-    port = 9000
+    host ='172.17.73.32'  # 或 'localhost' == '127.0.0.1'
+    port = 22
     server = yield from loop.create_server(app.make_handler(),host,port)
     logging.info('server start at http://%s:%s' % (host,port))
     return server
